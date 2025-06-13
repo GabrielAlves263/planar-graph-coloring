e92e61763266f0022684617dae5a862eea42822c
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-06-13 14:49:52 -0300

    feat: Implement graph coloring algorithms and update main to calculate chromatic number

--- src/main.cpp
+++ src/main.cpp
@@ -79,9 +79,53 @@ auto generatePlanarGraph(int n) -> pair<set<int>, vector<pair<int, int>>>
     return {vertices, edges};
 }
 
+bool isValidColoring(const vector<int> &color, const vector<int> &vertices, const vector<list<int>> &adj)
+{
+    for (size_t i = 0; i < vertices.size(); i++)
+    {
+        for (const auto &neighbor : adj[i])
+        {
+            int j = find(vertices.begin(), vertices.end(), neighbor) - vertices.begin();
+            if (color[i] == color[j])
+                return false;
+        }
+    }
+    return true;
+}
+
+bool bruteForceColoring(const vector<int> &vertices, const vector<list<int>> &adj, int k, vector<int> &color, size_t pos = 0)
+{
+    if (pos == vertices.size())
+    {
+        return isValidColoring(color, vertices, adj);
+    }
+
+    for (int c = 1; c <= k; c++)
+    {
+        color[pos] = c;
+        if (bruteForceColoring(vertices, adj, k, color, pos + 1))
+            return true;
+    }
+    return false;
+}
+
+int findChromaticNumber(const vector<int> &vertices, const vector<list<int>> &adj)
+{
+    for (size_t k = 1; k <= vertices.size(); k++)
+    {
+        vector<int> color(vertices.size(), 0);
+        if (bruteForceColoring(vertices, adj, k, color))
+        {
+            return k;
+        }
+    }
+
+    return -1;
+}
+
 int main()
 {
-    int n = 500;
+    int n = 30;
     auto planarGraph = generatePlanarGraph(n);
     auto vertices = planarGraph.first;
     auto edges = planarGraph.second;
@@ -101,7 +145,11 @@ int main()
         graphMatrix.insertEdge(a.first, a.second);
     }
 
-    graphMatrix.printGraph();
+    graphList.printGraph();
+
+    int chromaticNumber = findChromaticNumber(graphList.getVertices(), graphList.getAdj());
+
+    cout << "Chromatic Number: " << chromaticNumber << endl;
 
     return 0;
 }
